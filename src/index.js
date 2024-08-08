73aa9a46a98838dcb55e5075f2ce28e42f9a70dd
Isvaya <tasyana.pulatova@gmail.com>
2024-08-08 16:09:42 +0500

    docs: change index js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
-}
+  let result = 0;
+  n = Math.abs(n);
+
+  while (n > 0) {
+    const lastDigit = n % 10;
+    result = result * 10 + lastDigit;
+    n = Math.floor(n / 10);
+  }
+
+
+  return result;
+
+};
